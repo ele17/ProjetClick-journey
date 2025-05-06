fd5c9f0681549b5f80b29f42e2c9c4760876f2e9
ele17 <131241638+ele17@users.noreply.github.com>
2025-05-06 13:43:35 +0200

    Update supprimerpanier.php

--- ClickJourneyPHP_Final/pages/supprimerpanier.php
+++ ClickJourneyPHP_Final/pages/supprimerpanier.php
@@ -1,1 +1,12 @@
+<?php
+session_start();
+include 'header.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
+    $id = $_POST['id'];
+    unset($_SESSION['panier'][$id]);
+}
+
+header('Location: panier.php');
+exit;
 
